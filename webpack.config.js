9e896fedf58358235af645be398bc22992b02ecf
Hans Andersen <hans@helphouse.io>
2017-06-01 12:09:19 +0200

    Updated SEED to reflect the latest knowledge in webpack and ZAF development

--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,19 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const PRODUCTION = (process.env.NODE_ENV == 'production');
+const PRODUCTION = (process.env.NODE_ENV === 'production' || process.argv.indexOf('-p') > -1);
 
 const extractSCSS = new ExtractTextPlugin('[name].css');
 
 var externalAssets = {
   css: [
-    'https://assets.zendesk.com/apps/sdk-assets/css/0/zendesk_garden.css'
+    'https://assets.zendesk.com/apps/sdk-assets/css/1/zendesk_garden.css'
   ],
   js: [
     'https://cdn.jsdelivr.net/g/lodash@4.17.4,handlebarsjs@4.0.5,jquery@3.1.1',
     'https://assets.zendesk.com/apps/sdk/2.0/zaf_sdk.js'
   ]
 };
-
-const plugins = [
-  extractSCSS,
-  new HtmlWebpackPlugin({
-    warning: 'AUTOMATICALLY GENERATED FROM ./lib/templates/layout.hdbs - DO NOT MODIFY THIS FILE DIRECTLY',
-    vendorCss: externalAssets.css,
-    vendorJs: externalAssets.js,
-    template: './lib/templates/layout.hdbs',
-    filename: 'index.html'
-  })
-];
-
-if(PRODUCTION) {
-  // plugins.push(new webpack.optimize.UglifyJsPlugin({
-  //   compress: {
-  //     warnings: true
-  //   }
-  // }));
-}
 
 module.exports = {
   entry: {
@@ -60,7 +41,7 @@ module.exports = {
         }
       },
       {
-        test: /\.json$/,
+        test: /translations\/.+\.json$/,
         include: path.resolve(__dirname, './src/translations'),
         use: {
           loader: 'translations-loader',
@@ -75,24 +56,28 @@ module.exports = {
           fallback: 'style-loader',
           use: [
             { loader: 'css-loader' },
-            { loader: 'sass-loader' }
+            { loader: 'sass-loader',
+              options: {
+                includePaths: ["node_modules/compass-mixins/lib"]
+              }
+            }
           ]
         })
       },
       {
-        test: /\.(js|es6)$/,
+        test: /\.es6$/,
         exclude: /node_modules/,
         use: [
           {
             loader: 'babel-loader',
             options: {
-              presets: ['es2015']
+              presets: ['latest']
             }
           }
         ]
       },
       {
-        test: /\.hdbs/,
+        test: /\.hdbs$/,
         use: {
           loader: "handlebars-loader",
           options: {
@@ -103,7 +88,7 @@ module.exports = {
         }
       },
       {
-        test: /fonts\/.+\.(svg|woff|woff2|ttf|eot)$/,
+        test: /fonts\/.+\.(woff|woff2|ttf|eot)$/,
         use: {
           loader: 'file-loader',
           options: {
@@ -137,6 +122,18 @@ module.exports = {
     lodash: '_',
     zendesk_app_framework_sdk: 'ZAFClient'
   },
-  devtool: "source-map",
-  plugins: plugins
+  plugins: [
+    extractSCSS,
+    new HtmlWebpackPlugin({
+      warning: 'AUTOMATICALLY GENERATED FROM ./lib/templates/layout.hdbs - DO NOT MODIFY THIS FILE DIRECTLY',
+      vendorCss: externalAssets.css,
+      vendorJs: externalAssets.js,
+      template: './lib/templates/layout.hdbs',
+      filename: 'index.html'
+    })
+  ]
 };
+
+if(!PRODUCTION) {
+  module.exports["devtool"] = "source-map"
+}
